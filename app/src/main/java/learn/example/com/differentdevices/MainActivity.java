4d4766805e484ffb054b397024ec67fe14f96c9f
Tomas13 <tomas.mapoter@gmail.com>
2016-10-03 10:22:34 +0600

    Added ShortcutBadger

--- app/src/main/java/learn/example/com/differentdevices/MainActivity.java
+++ app/src/main/java/learn/example/com/differentdevices/MainActivity.java
@@ -5,15 +5,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import me.leolin.shortcutbadger.ShortcutBadger;
+
 public class MainActivity extends AppCompatActivity {
+
+    private int badgeCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
     public void startActivity(View view){
         startActivity(new Intent(this, SecondActivity.class));
+    }
+
+    public void updateBadge(View view){
+        ShortcutBadger.applyCount(this, ++badgeCount); //for 1.1.4+
     }
 }
